bf720f630d7df05863b2988960ea1337642567cb
ccryptoDev <92884804+ccryptoDev@users.noreply.github.com>
2022-11-01 13:00:04 -0400

    modified config

--- client/src/app.config.ts
+++ client/src/app.config.ts
@@ -1,6 +1,14 @@
 let baseUrl = "localhost:3000";
 const environment = process.env.NODE_ENV;
 
+if (
+  environment &&
+  environment === "development" &&
+  process.env.REACT_APP_BASE_URL
+) {
+  baseUrl = process.env.REACT_APP_BASE_URL;
+}
+
 if (environment && environment !== "development") {
   baseUrl = "localhost:3000";
 }
