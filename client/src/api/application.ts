5edbeb2455cf7ef7c3a51f9f702fcb903156d324
ccryptoDev <92884804+ccryptoDev@users.noreply.github.com>
2022-11-01 13:00:27 -0400

    modified application

--- client/src/api/application.ts
+++ client/src/api/application.ts
@@ -11,53 +11,79 @@ export type IResponse = {
 
 // Gets the User info
 // THIS IS AN EXAMPLE OF AN AUTHORIZED REQUEST
-export async function getUserInfo() {
+export async function getApplicationInfo(id: string) {
   let response: any = { data: null, error: null };
   try {
-    response = await getRequester().get(`${baseUrl}/ApiGetUserInfo`);
+    response = await axios.get(`${baseUrl}/application/${id}`);
   } catch (error) {
     response.error = error;
   }
   return response;
 }
 
-export async function fetchContractApi({
-  screenTrackingId,
-}: {
-  screenTrackingId: string;
-}) {
-  let response: IResponse = { data: null, error: null };
+interface IUpdateApplication {
+  requestedAmount?: number;
+  annualIncome?: number;
+  currentScreen: string;
+  screenId: string;
+}
+
+export async function updateApplicationInfo(body: IUpdateApplication) {
+  let response: any = { data: null, error: null };
+  const { screenId } = body;
   try {
-    response = await getRequester().get(
-      `${baseUrl}/api/application/contract `,
-      {
-        params: {
-          screenTrackingId,
-        },
-      }
-    );
+    response = await axios.patch(`${baseUrl}/application/${screenId}`, body);
   } catch (error) {
     response.error = error;
   }
   return response;
 }
 
-export async function finalizeContractApi({
-  screenTrackingId,
-  userId,
-}: {
+export async function reviewOfferApi(screenId: string) {
+  let response: any = { data: null, error: null };
+  try {
+    response = await axios.get(`${baseUrl}/aplication_flow/review_offer/1`);
+  } catch (error) {
+    response.error = error;
+  }
+  return response;
+}
+
+interface ISaveSignature {
   screenTrackingId: string;
-  userId: string;
-}) {
+  imgBase64: string;
+}
+
+export async function saveSignatureApi(body: ISaveSignature) {
+  let response: any = { data: null, error: null };
+  const { screenTrackingId, imgBase64 } = body;
+  try {
+    response = await axios.post(`${baseUrl}/application/esignature`, {
+      imgBase64,
+      screenTrackingId,
+    });
+  } catch (error) {
+    response.error = error;
+  }
+  return response;
+}
+
+// -----------------------------------------
+
+export async function fetchContractApi(screenTrackingId: string) {
   let response: IResponse = { data: null, error: null };
   try {
-    response = await getRequester().post(
-      `${baseUrl}/api/application/finalize  `,
-      {
-        screenTrackingId,
-        userId,
-      }
-    );
+    response = await axios.get(`${baseUrl}/application/contract`);
+  } catch (error) {
+    response.error = error;
+  }
+  return response;
+}
+
+export async function finalizeContractApi(screenTrackingId: string) {
+  let response: IResponse = { data: null, error: null };
+  try {
+    response = await getRequester().post(`${baseUrl}/application/consents  `);
   } catch (error) {
     response.error = error;
   }
