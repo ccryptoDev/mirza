e74bb086dd34fc9d03c500380edaec0feaa155dd
ccryptoDev <92884804+ccryptoDev@users.noreply.github.com>
2022-11-01 13:03:46 -0400

    updated childcare

--- client/src/pages/los/childcare.jsx
+++ client/src/pages/los/childcare.jsx
@@ -2,24 +2,28 @@ import React from "react";
 import { useHistory } from "react-router-dom";
 import Layout from "../../layouts/LOS";
 import ProgressBar from "../../components/templates/LOS/ProgressBar";
-import { routes } from "../../routes/los/routes";
+import { routes, pageName } from "../../routes/los/routes";
 import { ContainerInternal as Container } from "../../layouts/LOS/Containers";
 import { ReactComponent as Img } from "../../assets/svgs/illustrations/mother-money.svg";
 import Form from "../../components/templates/LOS/Childcare/Form";
+import PrivateRoute from "../../routes/los/PrivateRoute";
+import { useUserData } from "../../contexts/user";
 
 const Childcare = () => {
-  const history = useHistory();
+  const { goToPage } = useUserData();
   const goBackHandler = () => {
-    history.push(routes.INCOME);
+    goToPage(pageName.INCOME);
   };
   return (
-    <Layout goBackHandler={goBackHandler}>
-      <ProgressBar currentRoute={routes.CHILDCARE} />
-      <Container>
-        <Form />
-        <Img />
-      </Container>
-    </Layout>
+    <PrivateRoute route={routes.CHILDCARE}>
+      <Layout goBackHandler={goBackHandler}>
+        <ProgressBar currentRoute={routes.CHILDCARE} />
+        <Container>
+          <Form />
+          <Img />
+        </Container>
+      </Layout>
+    </PrivateRoute>
   );
 };
 
