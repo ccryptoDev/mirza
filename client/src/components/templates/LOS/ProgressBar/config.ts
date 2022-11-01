2d8784bfe06e44927065205559caf7aba1da38e1
ccryptoDev <92884804+ccryptoDev@users.noreply.github.com>
2022-11-01 13:02:36 -0400

    modified progressbar

--- client/src/components/templates/LOS/ProgressBar/config.ts
+++ client/src/components/templates/LOS/ProgressBar/config.ts
@@ -4,7 +4,7 @@ export const steps = [
   { route: routes.INCOME, number: 1, id: "1" },
   { route: routes.CHILDCARE, number: 2, id: "2" },
   { route: routes.PRE_APPROVAL, number: 3, id: "3" },
-  { route: routes.FORGIVENESS, number: 4, id: "4" },
+  { route: routes.FORGIVENESS_FULL, number: 4, id: "4" },
   { route: routes.FINANCING_APPLY, number: 5, id: "5" },
   { route: routes.OFFER, number: 6, id: "6" },
   { route: routes.CONTRACT, number: 7, id: "7" },
