4765189bf2fbb053b4fce59937f0e25f6d06c82c
ccryptoDev <92884804+ccryptoDev@users.noreply.github.com>
2022-11-02 10:14:36 -0400

    modified los in client

--- client/src/pages/los/income.jsx
+++ client/src/pages/los/income.jsx
@@ -5,19 +5,19 @@ import { routes } from "../../routes/los/routes";
 import { ContainerInternal as Container } from "../../layouts/LOS/Containers";
 import { ReactComponent as Img } from "../../assets/svgs/illustrations/grow-money-3.svg";
 import Form from "../../components/templates/LOS/Income/Form";
+import PrivateRoute from "../../routes/los/PrivateRoute";
 
 const Income = () => {
-  const goBackHandler = () => {
-    //
-  };
   return (
-    <Layout goBackHandler={goBackHandler}>
-      <ProgressBar currentRoute={routes.INCOME} />
-      <Container>
-        <Form />
-        <Img />
-      </Container>
-    </Layout>
+    <PrivateRoute route={routes.INCOME}>
+      <Layout>
+        <ProgressBar currentRoute={routes.INCOME} />
+        <Container>
+          <Form />
+          <Img />
+        </Container>
+      </Layout>
+    </PrivateRoute>
   );
 };
 
--- client/src/pages/los/pre-approval.jsx
+++ client/src/pages/los/pre-approval.jsx
@@ -1,25 +1,30 @@
 import React from "react";
-import { useHistory } from "react-router-dom";
+
 import Layout from "../../layouts/LOS";
 import ProgressBar from "../../components/templates/LOS/ProgressBar";
-import { routes } from "../../routes/los/routes";
+import { routes, pageName } from "../../routes/los/routes";
 import { ContainerInternal as Container } from "../../layouts/LOS/Containers";
 import { ReactComponent as Img } from "../../assets/svgs/illustrations/family-money.svg";
 import Form from "../../components/templates/LOS/PreApproval/Form";
+import { useUserData } from "../../contexts/user";
+import PrivateRoute from "../../routes/los/PrivateRoute";
 
 const PreApproval = () => {
-  const history = useHistory();
+  const { goToPage } = useUserData();
+
   const goBackHandler = () => {
-    history.push(routes.CHILDCARE);
+    goToPage(pageName.CHILDCARE);
   };
   return (
-    <Layout goBackHandler={goBackHandler}>
-      <ProgressBar currentRoute={routes.PRE_APPROVAL} />
-      <Container>
-        <Form />
-        <Img />
-      </Container>
-    </Layout>
+    <PrivateRoute route={routes.PRE_APPROVAL}>
+      <Layout goBackHandler={goBackHandler}>
+        <ProgressBar currentRoute={routes.PRE_APPROVAL} />
+        <Container>
+          <Form />
+          <Img />
+        </Container>
+      </Layout>
+    </PrivateRoute>
   );
 };
 
